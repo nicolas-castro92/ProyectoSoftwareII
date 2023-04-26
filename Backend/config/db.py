c0c1d3a7701665d96f51bd8d11dda0dd9bf98c41
Nicolás Castro Giraldo <nicolas.castro9219@gmail.com>
2023-04-26 08:12:27 -0500

    add db configuracion

---
+++ Backend/config/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine, MetaData
+
+engine = create_engine("mysql+pymysql://admin:12345@localhost:3306/HOSPITALIZACION")
+
+conn = engine.connect()
+
+meta_data = MetaData()
