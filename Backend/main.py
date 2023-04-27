2997bac9914ac583e12d57b3821b9c6b1f154e4f
Nicolás Castro Giraldo <nicolas.castro9219@gmail.com>
2023-04-26 21:32:21 -0500

    preparando main para implementar casos de uso

--- Backend/main.py
+++ Backend/main.py
@@ -4,3 +4,9 @@ from router.user_router import user
 app = FastAPI()
 
 app.include_router(user)
+
+# INSTANCIA CASOS DE USO ITERACION 1
+# caso de uso Jose-Davids, aqui instancia el router
+# caso de uso Estefania-Nicolas, aqui instancia el router
+# caso de uso Alejandro, aqui instancia el router
+
