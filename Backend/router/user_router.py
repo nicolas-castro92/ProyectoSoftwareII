ad02d07fe181898f7cc0d81ffaf4e34ce9e1016e
Nicolás Castro Giraldo <nicolas.castro9219@gmail.com>
2023-04-26 20:11:42 -0500

    agregando http status 201

--- Backend/router/user_router.py
+++ Backend/router/user_router.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from starlette.status import HTTP_201_CREATED
 from schema.user_schema import UserSchema
 from config.db import conn
 from model.user import users
 
 user = APIRouter()
 
-@user.post("/api/user")
+@user.post("/api/user", status_code=HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
     nuevo = data_user.dict()
     conn.execute(users.insert().values(nuevo))
     conn.commit()
     print(data_user)
     print(nuevo)
-    return "Sucess"
+    return Response(status_code=HTTP_201_CREATED)
