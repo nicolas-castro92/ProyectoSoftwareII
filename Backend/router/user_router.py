a7879ef42b16072587800c32042ec5e55e79bd7c
Nicolás Castro Giraldo <nicolas.castro9219@gmail.com>
2023-04-26 20:36:01 -0500

    creando consulta para verificar que un usuario nuevo no este previamente registrado en la base de datos

--- Backend/router/user_router.py
+++ Backend/router/user_router.py
@@ -1,17 +1,28 @@
 from fastapi import APIRouter, Response
-from starlette.status import HTTP_201_CREATED
+from starlette.status import HTTP_201_CREATED, HTTP_409_CONFLICT
 from schema.user_schema import UserSchema
 from config.db import engine
 from model.user import users
 
 user = APIRouter()
 
+
 @user.post("/api/user", status_code=HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
     with engine.connect() as conn:
+        # Check if the ID is already in the database
+        query = users.select().where(users.c.identification_card == data_user.identification_card)
+        result = conn.execute(query)
+        existing_user = result.fetchone()
+
+        if existing_user:
+            # Return an HTTP 409 Conflict response if the ID already exists
+            return Response(status_code=HTTP_409_CONFLICT,content="User already exists")
+
+        # Insert the new user if the ID is not already in the database
         nuevo = data_user.dict()
         conn.execute(users.insert().values(nuevo))
         conn.commit()
-        print(data_user)
-        print(nuevo)
-        return Response(status_code=HTTP_201_CREATED)
+
+    # Return an HTTP 201 Created response if the user was successfully created
+    return Response(status_code=HTTP_201_CREATED)
