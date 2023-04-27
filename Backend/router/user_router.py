c7f89233e4ebae39b5a35070300583b212fb05b1
Nicolás Castro Giraldo <nicolas.castro9219@gmail.com>
2023-04-26 20:15:27 -0500

    optimizando conexion a la base de datos cerrando con cada peticion

--- Backend/router/user_router.py
+++ Backend/router/user_router.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Response
 from starlette.status import HTTP_201_CREATED
 from schema.user_schema import UserSchema
-from config.db import conn
+from config.db import engine
 from model.user import users
 
 user = APIRouter()
 
 @user.post("/api/user", status_code=HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
-    nuevo = data_user.dict()
-    conn.execute(users.insert().values(nuevo))
-    conn.commit()
-    print(data_user)
-    print(nuevo)
-    return Response(status_code=HTTP_201_CREATED)
+    with engine.connect() as conn:
+        nuevo = data_user.dict()
+        conn.execute(users.insert().values(nuevo))
+        conn.commit()
+        print(data_user)
+        print(nuevo)
+        return Response(status_code=HTTP_201_CREATED)
