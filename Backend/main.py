b44eb237bc15e3635bd801030c88e1ca91cc4ff3
Alejandro Leal <alejandro.1701714615@ucaldas.edu.co>
2023-04-28 22:58:08 -0500

    Creacion de modelo, esquema y router del paciente

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from router.user_router import user
+from router.patient_router import patient
 
 app = FastAPI()
 
 app.include_router(user)
+app.include_router(patient)
 
 # INSTANCIA CASOS DE USO ITERACION 1
 # caso de uso Jose-Davids, aqui instancia el router
