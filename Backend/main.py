cda984044c7ff3528c22b0e08fa0defdfe15b272
Alejandro Leal <alejandro.1701714615@ucaldas.edu.co>
2023-04-28 23:14:20 -0500

    Solucion a conflicto en la main

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 
 app.include_router(user)
 
-
 # INSTANCIA CASOS DE USO ITERACION 1
 # caso de uso Jose-Davids, aqui instancia el router
 # caso de uso Estefania-Nicolas, aqui instancia el router
