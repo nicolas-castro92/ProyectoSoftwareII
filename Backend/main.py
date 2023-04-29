96846c8a050e64a8eba26f27c367a3cfb77151e3
Alejandro Leal <alejandro.1701714615@ucaldas.edu.co>
2023-04-28 23:07:37 -0500

    Modificacion de la main para el router

--- Backend/main.py
+++ Backend/main.py
@@ -5,10 +5,10 @@ from router.patient_router import patient
 app = FastAPI()
 
 app.include_router(user)
-app.include_router(patient)
+
 
 # INSTANCIA CASOS DE USO ITERACION 1
 # caso de uso Jose-Davids, aqui instancia el router
 # caso de uso Estefania-Nicolas, aqui instancia el router
-# caso de uso Alejandro, aqui instancia el router
+app.include_router(patient)
 
