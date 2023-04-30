668ac54cee7f30c9947574d855da19c5aa1b0af0
DavidSalazarG <david.1701713821@ucaldas.edu.co>
2023-04-30 16:15:35 -0500

    Arreglo detalles de la creación del personal medico

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://admin:12345@localhost:3306/HOSPITALIZACION")
+engine = create_engine("mysql+pymysql://admin:12345@localhost:3306/prueba")
 
 meta_data = MetaData()
