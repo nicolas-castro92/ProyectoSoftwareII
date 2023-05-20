1314bb84d9fb74afcade178082fb76f4c7d9ab67
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 18:41:22 -0500

    add medical_staff to main route

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from router.user import router as user_router
 from router.familiar import router as familiar_router
+from router.medical_staff import router as medical_staff_router
 
 app = FastAPI()
 
 # Routers
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(familiar_router, prefix="/familiars", tags=["familiars"])
+app.include_router(medical_staff_router, prefix="/medical_staff", tags=["medical_staff"])
 
---
+++ Backend/router/medical_staff.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from config.database import get_db
+from model.user import User
+from model.medical_staff import MedicalStaff
+from schema.medical_staff import UserMedicalStaffCreate, UserMedicalStaffView
+from schema.user import UserCreate, UserView
+from config.database import SessionLocal
+import random
+import string
+from typing import List
+
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/create_user_with_medical_staff", response_model=UserMedicalStaffView)
+def create_user_with_medical_staff(user_medical_staff: UserMedicalStaffCreate, db: Session = Depends(get_db)):
+    # Verificar si el email ya está en uso
+    if db.query(User).filter(User.email == user_medical_staff.email).first():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already in use")
+    
+    # Verificar si la identification_card ya está en uso
+    if db.query(User).filter(User.identification_card == user_medical_staff.identification_card).first():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Identification card already in use")
+
+    # Generar una contraseña aleatoria
+    password = generate_random_password()
+
+    # Crear el usuario primero
+    new_user = User(
+        name=user_medical_staff.name,
+        last_name=user_medical_staff.last_name,
+        identification_card=user_medical_staff.identification_card,
+        age=user_medical_staff.age,
+        phone=user_medical_staff.phone,
+        email=user_medical_staff.email,
+        password=password,
+        address=user_medical_staff.address,
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    # Crear el personal médico y asignar al usuario creado
+    new_medical_staff = MedicalStaff(
+        professional_card=user_medical_staff.professional_card,
+        specialty=user_medical_staff.specialty,
+        personal_type=user_medical_staff.personal_type,
+        user_id=new_user.id,
+    )
+    db.add(new_medical_staff)
+    db.commit()
+    db.refresh(new_medical_staff)
+
+    user_medical_staff_view = UserMedicalStaffView(
+        id=new_user.id,
+        name=new_user.name,
+        last_name=new_user.last_name,
+        identification_card=new_user.identification_card,
+        age=new_user.age,
+        phone=new_user.phone,
+        email=new_user.email,
+        address=new_user.address,
+        professional_card=new_medical_staff.professional_card,
+        specialty=new_medical_staff.specialty,
+        personal_type=new_medical_staff.personal_type,
+    )
+    return user_medical_staff_view
+
+
+def generate_random_password(length=5):
+    digits = string.digits
+    return ''.join(random.choice(digits) for _ in range(length))
