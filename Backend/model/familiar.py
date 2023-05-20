8f89f2fa0487f44c4f7d3d44422c3e28a16e080e
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 15:47:53 -0500

    add familiar model

---
+++ Backend/model/familiar.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from config.database import Base, engine
+
+class Familiar(Base):
+    __tablename__ = "familiars"
+
+    id = Column(Integer, primary_key=True, index=True)
+    alternate_phone = Column(String(255))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="familiars")
+
+Base.metadata.create_all(bind=engine)
