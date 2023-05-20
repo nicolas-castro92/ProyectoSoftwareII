c21b09de9926e68f04090d4936af5b5c4f498dfd
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 14:50:43 -0500

    add new feature to project

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://admin:12345@localhost:3306/HOSPITALIZACION")
+engine = create_engine("mysql+pymysql://root:12345@localhost:3306/borradordb")
 
 meta_data = MetaData()
--- Backend/schema/familiar_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from sqlalchemy import ForeignKey
-
-class FamiliarSchema(BaseModel):
-    id: Optional[int]
-    user_id: int 
-    alternate_phone: str
