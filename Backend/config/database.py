8164a8c6df545d47ad544a6503aa99dc303973ff
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 15:41:33 -0500

    add new features to backend

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/taskdb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/borradordb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
