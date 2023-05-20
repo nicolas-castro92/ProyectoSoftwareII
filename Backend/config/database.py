37ce781926c38ab084b56d60a9d2bdc7e989a81f
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 15:13:54 -0500

    update database archive

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -1,5 +1,10 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root:12345@localhost:3306/borradordb")
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/taskdb"
 
-meta_data = MetaData()
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
