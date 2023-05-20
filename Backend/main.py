fe0da8202bca8f9a9b7acb7ffeec4d5be6efb752
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 15:11:35 -0500

    delete main text

--- Backend/main.py
+++ Backend/main.py
@@ -1,16 +1,2 @@
 
-from router.medical_staff_router import medical_staff
-from fastapi import FastAPI
-from router.user_router import user
-from router.familiar_router import familiar
-from router.patient_router import patient
-
-app = FastAPI()
-
-app.include_router(user)
-
-# INSTANCIA CASOS DE USO ITERACION 1
-app.include_router(medical_staff)
-app.include_router(familiar)
-app.include_router(patient)
 
