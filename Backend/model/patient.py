844e43320a480f60671334e4ce7ac79ff60bd911
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 19:55:19 -0500

    add patient model

---
+++ Backend/model/patient.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from model.base import Base
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    familiar_id = Column(Integer, ForeignKey("familiars.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    familiar = relationship("Familiar", back_populates="patients")
+    user = relationship("User", back_populates="patients")
