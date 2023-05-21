74a516981b28e51671d91ff79e6498019d5323b0
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-05-20 22:16:59 -0500

    add functions to patient router

--- Backend/schema/patient.py
+++ Backend/schema/patient.py
@@ -1,6 +1,25 @@
 from pydantic import BaseModel
 from typing import Optional
 
+class UserPatientFamiliarView(BaseModel):
+    user_id: int
+    name: str
+    last_name: str
+    identification_card: str
+    age: int
+    phone: str
+    email: str
+    address: str
+    patient_id: int
+    familiar_id: Optional[int]
+    familiar_name: Optional[str]
+    familiar_last_name: Optional[str]
+    familiar_identification_card: Optional[str]
+    familiar_age: Optional[int]
+    familiar_phone: Optional[str]
+    familiar_email: Optional[str]
+    familiar_address: Optional[str]
+    familiar_alternate_phone: Optional[str]
 
 class UserPatientViewAll(BaseModel):
     name: str
@@ -40,6 +59,8 @@ class UserPatientUpdate(BaseModel):
     phone: str
     email: str
     address: str
+    familiar_id: Optional[int]
+    
 
 class PatientBase(BaseModel):
     familiar_id: int
