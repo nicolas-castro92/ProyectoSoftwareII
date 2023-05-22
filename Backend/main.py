d1427d52fa9f35846ad034e4d4c4162c2796c1b4
DavidSalazarG <david.1701713821@ucaldas.edu.co>
2023-05-22 14:48:46 -0500

    Se añade la ruta al del personal a cargo al main

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ from router.user import router as user_router
 from router.familiar import router as familiar_router
 from router.medical_staff import router as medical_staff_router
 from router.patient import router as patient_router
+from router.personal_in_charge import personal_in_charge
 app = FastAPI()
 
 # Routers
@@ -10,4 +11,5 @@ app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(familiar_router, prefix="/familiars", tags=["familiars"])
 app.include_router(medical_staff_router, prefix="/medical_staff", tags=["medical_staff"])
 app.include_router(patient_router, prefix="/patients", tags=["patients"])
+app.include_router(personal_in_charge, prefix="/personal_in_charge", tags=["personal_in_charge"])
 
