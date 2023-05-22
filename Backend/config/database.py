e2528d7c36c608cf4d316bc5c861f35bb6119398
Heosve <juan.1701723284@ucaldas.edu.co>
2023-05-22 13:21:37 -0500

    Se corrige la clave y contraseña de la conexion hacia la base de datos

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/borradordb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:12345@localhost:3306/HOSPITALIZACION"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
